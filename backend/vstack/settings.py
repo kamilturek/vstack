af327df61ba77cb4f808f67b246a9c5036307b30
Kamil Turek <tazzu@live.com>
2020-07-04 21:09:34 +0200

    Configure psycopg2 database engine

--- backend/vstack/settings.py
+++ backend/vstack/settings.py
@@ -60,8 +60,12 @@ WSGI_APPLICATION = 'vstack.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'vstack',
+        'USER': env('POSTGRES_USER'),
+        'PASSWORD': env('POSTGRES_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
