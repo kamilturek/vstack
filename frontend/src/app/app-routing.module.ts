a0b870bdabdb94870be8362009c6a26b3dafd966
Kamil Turek <kamil.turek@hotmail.com>
2020-07-08 22:33:07 +0200

    Finialize LoginComponent UI

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { DefaultComponent } from './layouts/default/default.component';
 import { DashboardComponent } from './modules/dashboard/dashboard.component';
 import { InstancesComponent } from './modules/instances/instances.component';
+import { LoginComponent } from './modules/login/login.component';
 
 
 const routes: Routes = [
+    {
+        path: 'login',
+        component: LoginComponent
+    },
     {
         path: '',
         component: DefaultComponent,
--- frontend/src/app/layouts/default/default.module.ts
+++ frontend/src/app/layouts/default/default.module.ts
@@ -1,29 +1,40 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatButtonModule } from '@angular/material/button'; 
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatInputModule } from '@angular/material/input';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { DefaultComponent } from './default.component';
 import { DashboardComponent } from '../../modules/dashboard/dashboard.component';
 import { InstancesComponent } from '../../modules/instances/instances.component';
+import { LoginComponent } from '../../modules/login/login.component';
 
-import { RouterModule } from '@angular/router';
 import { SharedModule } from '../../shared/shared.module';
-
-import { MatDividerModule } from '@angular/material/divider';
-import { MatSidenavModule } from '@angular/material/sidenav';
 
 
 @NgModule({
     declarations: [
         DefaultComponent,
         DashboardComponent,
-        InstancesComponent
+        InstancesComponent,
+        LoginComponent,
     ],
     imports: [
         CommonModule,
+        FlexLayoutModule,
+        MatButtonModule,
+        MatCardModule,
+        MatDividerModule,
+        MatInputModule,
+        MatSidenavModule,
+        MatToolbarModule,
         RouterModule,
         SharedModule,
-        MatDividerModule,
-        MatSidenavModule,
     ]
 })
 export class DefaultModule { }
