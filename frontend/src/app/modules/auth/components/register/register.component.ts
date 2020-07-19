c729eb60f7e856c052d2ff20b8d8085e7b173351
Kamil Turek <kamil.turek@nokia.com>
2020-07-19 10:40:51 +0200

    Replace tabs with spaces

--- frontend/src/app/modules/auth/components/register/register.component.ts
+++ frontend/src/app/modules/auth/components/register/register.component.ts
@@ -4,31 +4,31 @@ import { AuthService } from '../../auth.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
-	selector: 'app-register',
-	templateUrl: './register.component.html',
-	styleUrls: ['./register.component.scss']
+    selector: 'app-register',
+    templateUrl: './register.component.html',
+    styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
 
-	registerForm = new FormGroup({
-		username: new FormControl('', [Validators.required]),
-		password: new FormControl('', [Validators.required]),
-		passwordConfirmation: new FormControl('', [Validators.required])
-	});
+    registerForm = new FormGroup({
+        username: new FormControl('', [Validators.required]),
+        password: new FormControl('', [Validators.required]),
+        passwordConfirmation: new FormControl('', [Validators.required])
+    });
 
-	constructor(
-		private authService: AuthService,
-		private snackBar: MatSnackBar
-	) { }
+    constructor(
+        private authService: AuthService,
+        private snackBar: MatSnackBar
+    ) { }
 
-	ngOnInit(): void {
-	}
+    ngOnInit(): void {
+    }
 
-	onSubmit(): void {
-		if (this.registerForm.valid) {
+    onSubmit(): void {
+        if (this.registerForm.valid) {
 
-		} else {
-			this.snackBar.open('Please provide all required credentials.', 'Hide');
-		}
-	}
+        } else {
+            this.snackBar.open('Please provide all required credentials.', 'Hide');
+        }
+    }
 }
