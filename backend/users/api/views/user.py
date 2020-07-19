5bc0bab2e56fcedab60fc6b10ce13b65d51aa8a8
Kamil Turek <kamil.turek@nokia.com>
2020-07-19 22:21:46 +0200

    Enable Browsable API login

--- backend/users/api/views/user.py
+++ backend/users/api/views/user.py
@@ -1,4 +1,8 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.request import Request
+from rest_framework.response import Response
 
 from django.contrib.auth.models import User
 
@@ -8,3 +12,8 @@ from users.api.serializers import UserSerializer
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+    @action(detail=False, methods=['get'])
+    def this(self, request: Request) -> Response:
+        return Response(self.get_serializer(request.user).data)
--- backend/vstack/urls.py
+++ backend/vstack/urls.py
@@ -6,6 +6,7 @@ from django.urls import path, include
 from users.api.urls import urlpatterns as users_urlpatterns
 
 urlpatterns = [
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
     path('api/auth/', views.obtain_auth_token),
     path('api/users/', include(users_urlpatterns))
