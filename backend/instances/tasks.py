3464ac3b5a5a3638ca8f6b9f257d8ff4ac510fcf
Kamil Turek <kamil.turek@nokia.com>
2020-10-10 23:00:12 +0200

    Send notification about finished deployment

--- backend/instances/tasks.py
+++ backend/instances/tasks.py
@@ -3,22 +3,25 @@ from __future__ import absolute_import
 from celery import shared_task
 
 from instances.models import Instance
-from notifications.models import InstanceScheduledNotification
+from notifications.models import InstanceFinishedNotification, InstanceScheduledNotification
 
 
 @shared_task
 def run_instance(instance: Instance) -> None:
+    InstanceScheduledNotification.send(
+        instance.accessors[0],
+        instance
+    )
     try:
         instance.run()
     except Exception as e:
         instance.delete()
         raise e
     else:
-        InstanceScheduledNotification.send(
+        InstanceFinishedNotification.send(
             instance.accessors[0],
             instance
         )
-
 
 @shared_task
 def remove_instance(instance: Instance) -> None:
