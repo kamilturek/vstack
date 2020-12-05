07eb78622867d5ea6e8ae6d35105f392d8835017
Kamil Turek <kamil.turek@stxnext.pl>
2020-12-05 20:15:58 +0100

    Create Volume model test cases

--- backend/instances/signals.py
+++ backend/instances/signals.py
@@ -1,10 +1,15 @@
 from django.db.models.signals import pre_delete
 from django.dispatch import receiver
 
-from instances.models import Instance
+from instances.models import Instance, Volume
 
 
 @receiver(pre_delete, sender=Instance)
 def remove_instance(sender, instance, **kwargs):
     if instance.container_id:
         instance.remove()
+
+
+@receiver(pre_delete, sender=Volume)
+def remove_volume(sender, instance, **kwargs):
+    instance.remove()
--- backend/instances/test/test_signals.py
+++ backend/instances/test/test_signals.py
@@ -2,7 +2,7 @@ from unittest import mock
 
 from django.test import TestCase
 
-from instances.models import Image, Instance
+from instances.models import Image, Instance, Volume
 
 
 class InstanceSignalsTestCase(TestCase):
@@ -23,4 +23,13 @@ class InstanceSignalsTestCase(TestCase):
             container_id='id'
         )
         instance.delete()
+        mock_remove.assert_called_once()
+
+
+class VolumeSignalsTestCase(TestCase):
+
+    @mock.patch.object(Volume, 'remove')
+    def test_remove_on_delete(self, mock_remove):
+        volume = Volume.objects.create(name='name')
+        volume.delete()
         mock_remove.assert_called_once()
